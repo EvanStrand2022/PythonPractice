0fa60b72be24b0fbfb4eab1e6eb50bb0fb7cbd01
EvanStrand2022 <818584@mystma.org>
2021-09-13 09:38:47 -0500

    Top Five Movies

---
+++ main.py
@@ -0,0 +1,14 @@
+#Print 1st movie
+print("Bumblebee")
+
+#print 2nd movie
+print("Zach Snyder's Justice League")
+
+#print 3rd  movie
+print("Sonic The Hedgehog")
+
+#print 4th movie
+print("Batman Begins")
+
+#print 5th movie
+print("Revenge of the Sith")
