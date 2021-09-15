7be437e15122626ba0fab011c4870cdddf98f138
EvanStrand2022 <818584@mystma.org>
2021-09-15 09:13:07 -0500

    Added more top fives

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
+#define function
 def topFiveMovies():
     #print the first movie
     print("Bumblebee")
-
     #print second movie
     print("Sonic The Hedghehog")
-
     #print third movie
     print("Zach Snyder's Justice League")
-
     #print fourth movie
     print("Batman Begins")
-
     #print fifth movie
     print("Revenge of the Sith")
+    print("these are my top five favorite movies.")
 
-print("these are my top five favorite movies.")
+#define function
+def topFiveGames():
+    #print first game
+    print("Fire Emblem Three Houses")
+    #print second game
+    print("Persona 5 Royal")
+    #print third game
+    print("Minecraft")
+    #print fourth game
+    print("Nier Automata")
+    #print fifth game
+    print("The Legend of Zelda: Breath of the Wild")
+
+#define function
+def topFiveCharacters():
+    #print the first character
+    print("Edelgard von Hresvelg")
+    #print the second character
+    print("Megatron(IDW 1)")
+    #print third character
+    print("Anakain Skywalker")
+    #print fourth character
+    print("Makoto Niijima")
+    #print fifth character
+    print("Ahsoka Tano")
 topFiveMovies()
