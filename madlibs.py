15dd66893dcc5af32924d6b8596ef37b24687fa5
EvanStrand2022 <818584@mystma.org>
2021-09-16 09:43:33 -0500

    made a mad lib

---
+++ madlibs.py
@@ -0,0 +1,4 @@
+#define function
+def madLib(adjective1, noun1, adjective2, noun2, place1, adjective3, adjective4, pluralnoun1, vehicle1, adjective5, adjective6, pluralnoun2, pluralnoun3, adjective7, noun3, verb1, adjective8, verb2, pluralnoun4, pluralnoun5, typeofjob1, adjective9, verb3, adjective10):
+    #output string
+    print("Star Wars is a "+adjective1+" "+noun1+" of "+adjective2+" versus evil in a "+noun3+" ; "+place1+" far far away. ")
